53db244fc3504e9c6d7e123de7cf4ef7d7d96870
root <root@ponos.localdomain>
2017-11-20 18:09:59 +0800

    Initial commit

---
+++ rofi.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+rofi -config /usr/share/rofi/themes/custom.rasi -show-icons -font "Heebo 12" -lines 1 -columns 7 -modi "window,drun" -display-window "Switcher" -display-drun "Run" -window-format {t} -show window

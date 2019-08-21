063df942a9fbf252c2af283c35857aa3780b1f42
Deneir Uy <deneir@newlogic.io>
2019-08-21 10:34:42 +0800

    Update rofi

---
+++ rofi-emoji.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+rofi -config /usr/share/rofi/themes/custom.rasi -width 49 -show-icons -font "Poppins 14" -lines 10 -columns 2 -modi "emoji" -show emoji -monitor -4 -display-emoji "Emoji: "
--- rofi.sh
+++ rofi.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-rofi -config /usr/share/rofi/themes/custom.rasi -show-icons -font "Heebo 12" -lines 1 -columns 7 -modi "window,drun" -display-window "Switcher" -display-drun "Run" -window-format {t} -show window
+rofi -config /usr/share/rofi/themes/custom.rasi -width 55 -show-icons -font "Poppins 12" -lines 10 -columns 2 -modi "drun" -display-drun "Run: " -window-format {t} -show drun -monitor -1
